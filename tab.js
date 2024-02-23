fbb6b5d2e9ef630980a0a2b9ea591faa64ed8d49
vietphu2021 <vietphu@fastwork.vn>
2024-02-23 16:22:10 +0700

    d

--- tab.js
+++ tab.js
@@ -1,9 +1,9 @@
-console.log("Hello World");
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
+    tabcontent[i].className = tabcontent[i].className.replace(" active", "");
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
