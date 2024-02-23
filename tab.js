b5b8428f7a297642a963bbd99d9f7bf4f218cc98
vietphu2021 <vietphu@fastwork.vn>
2024-02-23 16:46:09 +0700

    Update tab.js

--- tab.js
+++ tab.js
@@ -15,12 +15,12 @@ function openCity(evt, cityName) {
 
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.display = "none";
-  }
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tabcontent = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
     tabcontent[i].className = tabcontent[i].className.replace(" active", "");
   }
   document.getElementById(cityName).style.display = "block";
