15d1a75af62f45f01f749f7ff68dad24c3caff2f
mrdesign94 <gentra123456789@gmail.com>
2024-02-23 20:22:20 +0700

    Update tab.js

--- tab.js
+++ tab.js
@@ -9,6 +9,27 @@ function openCity(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
+  tablinks = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tabcontent[i].className.replace(" active", "");
+  }
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+
+
+// console.log("Hello, World! tab-js");
+// function openCity(evt, cityName) {
+//   var i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//   document.getElementById(cityName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
