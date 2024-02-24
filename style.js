ed6eec2132eb6dd0f349a6507717cd6c40d5568e
vietphu2021 <vietphu@fastwork.vn>
2024-02-24 09:00:50 +0700

    Update style.js

--- style.js
+++ style.js
@@ -23,15 +23,15 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slide.Index = 1}
+  if (n < 1) {slide.Index = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slide.Index-1].style.display = "block";
+  dots[slide.Index-1].className += " active";
+  captionText.innerHTML = dots[slide.Index-1].alt;
 }
