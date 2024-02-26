a14fb0346d74f5ea9ae1ba7ca893e17880f5cf53
mrdesign94 <gentra123456789@gmail.com>
2024-02-26 22:33:08 +0700

    l

--- menu-mobile.js
+++ menu-mobile.js
@@ -1,5 +1,38 @@
-function $(document).ready(function() {
-  $('.menu__icon').click(function(){
-		$('body').toggleClass('menu_shown');
-	});
+jQuery(document).ready(function( $ ) {
+// menu mobile
+$(function() {
+  $(".e-mobile-menu__main-menu .menu-item-has-children > a").click(function(e) {
+    e.preventDefault();
+    $(this)
+      .next("ul")
+      .toggleClass("js-mobile-menu-open");
+    $(".e-mobile-menu__main-menu .menu-item-has-children > a")
+      .not(this)
+      .next("ul")
+      .removeClass("js-mobile-menu-open");
+    $(".e-mobile-menu__main-menu .menu-item-has-children > a")
+      .not(this)
+      .removeClass("js-menu-active");
+    $(this).toggleClass("js-menu-active");
+  });
+});
+// menu desktop
+$(function() {
+  $(".c-desktop-menu > div > ul > .menu-item-has-children > a").click(function(
+    e
+  ) {
+    e.preventDefault();
+    $(this)
+      .next("ul")
+      .toggleClass("js-menu-open");
+    $(".c-desktop-menu > div > ul > .menu-item-has-children > a")
+      .not(this)
+      .next("ul")
+      .removeClass("js-menu-open");
+    $(".c-desktop-menu > div > ul > .menu-item-has-children > a")
+      .not(this)
+      .removeClass("js-menu-active");
+    $(this).toggleClass("js-menu-active");
+  });
+});
 });
