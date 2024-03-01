94b3caf458e081d003e33243ee985c0f0feda89c
vietphu2021 <vietphu@fastwork.vn>
2024-03-01 14:35:47 +0700

    vvvv

--- tab.js
+++ tab.js
@@ -20,63 +20,7 @@ function openCity(evt, cityName) {
   // evt.currentTarget.className += " active";
 }
 
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   const links = document.querySelectorAll(".item_menu_css a");
-//
-//   links.forEach(link => {
-//     let slug = link.getAttribute("href").split("/").reverse()[0];
-//     if (window.location.href.indexOf(slug) > -0) {
-//       link.classList.add('active-css');
-//     }
-//
-//     if (window.location.href === link.getAttribute("href")) {
-//       link.classList.remove('active-css');
-//     }
-//   });
-// });
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   const links = document.querySelectorAll(".item_menu_css a");
-//
-//   links.forEach(link => {
-//     let slug = link.getAttribute("href").split("/").filter(Boolean).pop();
-//     let currentURL = window.location.href;
-//
-//     if (currentURL.includes(slug)) {
-//       link.classList.add('active-css');
-//     } else {
-//       link.classList.remove('active-css');
-//     }
-//   });
-// });
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   const links = document.querySelectorAll(".item_menu_css a");
-//
-//   links.forEach(link => {
-//     link.addEventListener("click", function(e) {
-//       e.preventDefault(); // Ngăn chặn hành động mặc định của liên kết
-//       links.forEach(item => item.classList.remove("active-css"));
-//       this.classList.add("active-css");
-//       window.sessionStorage.setItem("activeLink", this.getAttribute("href")); // Lưu trạng thái "active"
-//     });
-//   });
-//
-//   // Phục hồi trạng thái "active" khi tải lại trang
-//   const activeLink = window.sessionStorage.getItem("activeLink");
-//   if (activeLink) {
-//     links.forEach(link => {
-//       if (link.getAttribute("href") === activeLink) {
-//         link.classList.add("active-css");
-//       }
-//     });
-//   }
-// });
-
+//active link menu
 document.addEventListener("DOMContentLoaded", function() {
   const links = document.querySelectorAll(".item_menu_css a");
   const activeLink = sessionStorage.getItem("activeLink");
