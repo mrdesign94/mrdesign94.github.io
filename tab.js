7d9eb15c8ba45e85414b8cb124d3220f18a497b8
vietphu2021 <vietphu@fastwork.vn>
2024-03-01 11:52:46 +0700

    Update tab.js

--- tab.js
+++ tab.js
@@ -30,7 +30,8 @@ document.addEventListener("DOMContentLoaded", function() {
     if (window.location.href.indexOf(slug) > -1) {
       link.classList.add('active-css');
     }
-    else {
+    let slug = link.getAttribute("href").split("https://mrdesign94.github.io").reverse()[0];
+    if (window.location.href.indexOf(slug) = 0) {
       link.classList.remove('active-css');
     }
 
@@ -39,6 +40,34 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 });
+
+
+//
+//
+// // Get url current page
+// var currentUrl = window.location.href;
+//
+// // Remove slash ("/") from the current page's URL
+// if (currentUrl.slice(-1) === "/") {
+//   currentUrl = currentUrl.slice(0, -1);
+// }
+//
+// // Select all <a> tags in the vertical menu
+// var menuLinks = document.querySelectorAll('.vertical_nav a');
+//
+// // Iterate through all the links in the menu and add class "active" if the URL of the link matches the URL of the current page
+// menuLinks.forEach(function(link) {
+//   var linkUrl = link.href;
+//
+//   // Remove slash ("/") from the current page's URL
+//   if (linkUrl.slice(-1) === "/") {
+//     linkUrl = linkUrl.slice(0, -1);
+//   }
+//
+//   if (linkUrl === currentUrl) {
+//     link.classList.add('active');
+//   }
+// });
 
 
 // console.log("Hello, World! tab-js");
