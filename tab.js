dd91e28a6af5805d831f7ece6f552fcb7301e7c1
vietphu2021 <vietphu@fastwork.vn>
2024-03-01 15:24:23 +0700

    Update tab.js

--- tab.js
+++ tab.js
@@ -41,29 +41,29 @@ function openCity(evt, cityName) {
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  const links = document.querySelectorAll(".item_menu_css a");
-  const logo = document.querySelector(".logo-css");
-  const activeLink = sessionStorage.getItem("activeLink");
+	const links = document.querySelectorAll(".item_menu_css a");
+	const logo = document.querySelector(".logo-css");
+	const activeLink = sessionStorage.getItem("activeLink");
 
-  links.forEach(link => {
-    if (link.getAttribute("href") === activeLink) {
-      link.classList.add("active-css");
-    }
+	links.forEach(link => {
+		if (link.getAttribute("href") === activeLink) {
+			link.classList.add("active-css");
+		}
 
-    link.addEventListener("click", function(e) {
-      links.forEach(item => item.classList.remove("active-css"));
-      this.classList.add("active-css");
-      sessionStorage.setItem("activeLink", this.getAttribute("href"));
-    });
-  });
+		link.addEventListener("click", function(e) {
+			links.forEach(item => item.classList.remove("active-css"));
+			this.classList.add("active-css");
+			sessionStorage.setItem("activeLink", this.getAttribute("href"));
+		});
+	});
 
-  logo.addEventListener("click", function(e) {
-    e.preventDefault(); // Ngăn chặn hành động mặc định của liên kết
-    links.forEach(item => item.classList.remove("active-css")); // Xóa lớp active từ tất cả liên kết
-    const homeLink = document.querySelector('nav.menu a[href="home"]'); // Tìm liên kết trang chủ
-    homeLink.classList.add("active-css"); // Thêm lớp active vào liên kết trang chủ
-    sessionStorage.setItem("activeLink", "home"); // Lưu trạng thái "active" cho trang chủ
-  });
+	logo.addEventListener("click", function(e) {
+		e.preventDefault(); // Ngăn chặn hành động mặc định của liên kết
+		links.forEach(item => item.classList.remove("active-css")); // Xóa lớp active từ tất cả liên kết
+		const homeLink = document.querySelector('nav.menu a[href="home"]'); // Tìm liên kết trang chủ
+		homeLink.classList.add("active-css"); // Thêm lớp active vào liên kết trang chủ
+		sessionStorage.setItem("activeLink", "home"); // Lưu trạng thái "active" cho trang chủ
+	});
 });
 
 
